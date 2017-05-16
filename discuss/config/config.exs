f60eb3d51d2fdcccc7bddbb59c26222269b0a149
Jeroen Van den Bossche <vanbosse.jeroen@gmail.com>
2017-05-16 21:11:34 +0200

    Github Oauth.

--- discuss/config/config.exs
+++ discuss/config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "59561cd1507758bae2a4",
+  client_secret: "d744121d8e1673d0ab6f64a1fa98848f599032c3"
